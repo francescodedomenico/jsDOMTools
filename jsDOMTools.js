139d63a9bc64d4d227250abc068e2ddf0a0c04cb
Francesco De Domenico <frankdd89@hotmail.it>
2012-10-08 19:25:55 +0200

    added getElementByClassName function

--- jsDOMTools.js
+++ jsDOMTools.js
@@ -5,6 +5,16 @@ function removeElementByID(id){
     var elem = document.getElementById(id);
     if (elem != null) elem.parentNode.removeChild(elem);
 }
+function getElementByClassName(tagName, className) {
+    var allTagNodes = document.getElementsByTagName(tagName);
+    if (allTagNodes != null) {
+        for (var i = 0, il = allTagNodes.length; i < il; i++) {
+            if (allTagNodes[i].getAttribute("class") == className)
+                return allTagNodes[i];
+        }
+    }
+    return null;
+}
 function removeElementByClassName(tagName,className){
 	var allTagNodes = document.getElementsByTagName(tagName);
 	if(allTagNodes!=null){
