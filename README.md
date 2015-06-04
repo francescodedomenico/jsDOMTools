92c16475d7cb8ff5d88ea5645368aba8692d6535
Francesco De Domenico <frankdd89@hotmail.it>
2015-06-04 22:40:42 +0200

    Deleted deprecated functions and fixed the readme

--- README.md
+++ README.md
@@ -10,11 +10,6 @@ description: searches for the toSearchString in string
 parameters: String string, the string to search in. String toSearchString, the string to search for in string.
 returns: true if the string is found, false otherwise
 
-function getElementsWithClassName(className)
-description: searches for every element cointaining the className substring in the "class" attribute.
-parameters:  String className -  the class substring we want elements to be returned.
-returns: and array of elements containing the className in the "class" attribute.
-
 function getTextBetween(Text,stringLeft,stringRight)
 description: returns the substring of Text between stringLeft and stringRight
 parameters: String Text - the source text; String stringLeft - left substring to search; String stringRight - right substring to the returned buffer.
@@ -25,13 +20,13 @@ description: removes the node idToRemove from the DOM
 parameters: String idToRemove, the id of the node to remove from the DOM.
 returns: void.
 
-function removeElementByClassName(nodeType,classNameToRemove)
+function removeElementsByClassName(nodeType,classNameToRemove)
 description: removes the node which has the classNameToRemove from the DOM
 parameters:	String nodeType, the Type of the node to be removed. eg: "div","h","a".
 			String classNameToRemove, the className of the node to be removed.
 returns: void.
 
-function removeElementByClassName(nodeType,classNameToRemove)
+function hideElementsByClassName(nodeType,classNameToRemove)
 description: set the visibility = hidden property to the node which has the classNameToRemove from the DOM
 parameters:	String nodeType, the Type of the node to hide. eg: "div","h","a".
 			String classNameToRemove, the className of the node to hide.
--- jsDOMTools.js
+++ jsDOMTools.js
@@ -1,88 +1,74 @@
-//jsDOMTools v0.4
+//jsDOMTools v0.5
 //author: Francesco De Domenico
 //website: https://github.com/frankdd89/jsDOMTools
-function removeElementByID(id){
-    var elem = document.getElementById(id);
-    if (elem != null) elem.parentNode.removeChild(elem);
+
+function removeElementByID(id) {
+  var elem = document.getElementById(id);
+  if (elem != null) elem.parentNode.removeChild(elem);
 }
-function containsString(str, toSearch){
-    for(var x=0;x<str.length;x++){
-      if(str.substring(x,x+toSearch.length)==toSearch){
-        return true;
+
+function containsString(str, toSearch) {
+  for (var x = 0; x < str.length; x++) {
+    if (str.substring(x, x + toSearch.length) == toSearch) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function getTextBetween(text, str1, str2) {
+  for (var x = 0; x < text.length; x++) {
+    if (text.substring(x, x + str1.length) == str1) {
+      x = x + str1.length;
+      var buffer = "";
+      for (var y = x; y < text.length; y++) {
+        if (text.substring(y, y + str2.length) == str2)
+          return buffer;
+        buffer += text[y];
       }
     }
-    return false;
   }
-  function getTextBetween(text,str1,str2){
-    for(var x=0;x<text.length;x++){
-      if(text.substring(x,x+str1.length)==str1){
-        x=x+str1.length;
-        var buffer = "";
-        for(var y=x;y<text.length;y++){
-          if(text.substring(y,y+str2.length)==str2)
-            return buffer;
-            buffer+=text[y];
-        }
-      }
-    }
-    return buffer;
-  }
-  function getElementsWithClassName(className){
-    var allNodes = document.getElementsByTagName("html")[0].childNodes;
-    var toReturn = new Array();
-    for(var i=0;i<allNodes.length;i++){
-      var tempClass = allNodes[i].getAttribute("class");
-      if(containsString(tempClass,className))
-        toReturn.append(allNodes[i]);
-    }
-    return toReturn;
-  }
-function getElementByClassName(tagName, className) {
-		var allTagNodes = document.getElementsByTagName(tagName);
-		var ret = new Array();
-		var cont = 0;
-		if (allTagNodes != null) {
-			for (var i = 0, il = allTagNodes.length; i < il; i++) {
-				if (allTagNodes[i].getAttribute("class")==className){
-						ret[cont] = allTagNodes[i];
-						cont++;
-					}
-			}
-		}
-		return ret;
-	}
-function removeElementByClassName(tagName,className){
-	var allTagNodes = document.getElementsByTagName(tagName);
-	if(allTagNodes!=null){
-		for (var i=0, il=allTagNodes.length; i<il; i++) {
-			if(allTagNodes[i].getAttribute("class")==className)
-				allTagNodes[i].parentNode.removeChild(allTagNodes[i]);
-		}
-	}
+  return buffer;
 }
-function hideElementByClassName(tagName,className){
-	var allTagNodes = document.getElementsByTagName(tagName);
-	if(allTagNodes!=null){
-		for (var i=0, il=allTagNodes.length; i<il; i++) {
-			if(allTagNodes[i].getAttribute("class")==className)
-				allTagNodes[i].style.visibility="hidden";
-		}
-	}
+
+function removeElementByClassName(tagName, className) {
+  var allTagNodes = document.getElementsByTagName(tagName);
+  if (allTagNodes != null) {
+    for (var i = 0, il = allTagNodes.length; i < il; i++) {
+      if (allTagNodes[i].getAttribute("class") == className)
+        allTagNodes[i].parentNode.removeChild(allTagNodes[i]);
+    }
+  }
 }
-function sendNotification(img,title,text) {
+
+function hideElementsByClassName(tagName, className) {
+  var allTagNodes = document.getElementsByTagName(tagName);
+  if (allTagNodes != null) {
+    for (var i = 0, il = allTagNodes.length; i < il; i++) {
+      if (allTagNodes[i].getAttribute("class") == className)
+        allTagNodes[i].style.visibility = "hidden";
+    }
+  }
+}
+
+function sendNotification(img, title, text) {
   if (!("Notification" in window)) {
     alert("This browser does not support desktop notification");
-  }
-  else if (Notification.permission === "granted") {
-    var notification = new Notification(title,{ body: text, icon: img });
-  }
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      if(!('permission' in Notification)) {
+  } else if (Notification.permission === "granted") {
+    var notification = new Notification(title, {
+      body: text,
+      icon: img
+    });
+  } else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function(permission) {
+      if (!('permission' in Notification)) {
         Notification.permission = permission;
       }
       if (permission === "granted") {
-        var notification = new Notification(title,{ body: text, icon: img });
+        var notification = new Notification(title, {
+          body: text,
+          icon: img
+        });
       }
     });
   }
